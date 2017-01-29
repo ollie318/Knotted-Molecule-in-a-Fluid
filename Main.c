4fa5eb36f5024942dc2a7daa2bd708eb798534b5
ollie318 <ow14948@my.bristol.ac.uk>
2017-01-29 13:08:25 +0000

    Changed back a few points that were changed accidentally

--- Main.c
+++ Main.c
@@ -233,21 +233,21 @@ FENE FENEForce(POSITION nMinusOnePos, POSITION nPos, POSITION nPosPlusOne, CONST
 
   double Q_x1 = nPosPlusOne.xPos - nPos.xPos;
 //Qs are current spring length, non-equilibrium
-  FENEForces.FENE_x1 = (c.H * Q_x1) / (1 - ( pow(Q_x1, 2) / pow(c.Q_0, 2)));                             //Q_x1 is bond length for x & right, x2 left, y in y-dir etc
+  FENEForces.FENE_x1 = (c.H * Q_x1) / (1 - ( pow(Q_x1, 2) / pow(c.MaxExtension, 2)));                             //Q_x1 is bond length for x & right, x2 left, y in y-dir etc
   double Q_x2 = nPos.xPos - nMinusOnePos.xPos;
-  FENEForces.FENE_x2 = (c.H * Q_x2) / (1 - ( pow(Q_x2, 2) / pow(c.Q_0, 2)));                           //Values usually between -10 & 10 -- -297 on one run?
+  FENEForces.FENE_x2 = (c.H * Q_x2) / (1 - ( pow(Q_x2, 2) / pow(c.MaxExtension, 2)));                           //Values usually between -10 & 10 -- -297 on one run?
 
   double Q_y1 = nPosPlusOne.yPos - nPos.yPos;
-  FENEForces.FENE_y1 = (c.H * Q_y1) / (1 - (pow(Q_y1, 2) / pow (c.Q_0, 2)));
+  FENEForces.FENE_y1 = (c.H * Q_y1) / (1 - (pow(Q_y1, 2) / pow (c.MaxExtension, 2)));
 
   double Q_y2 = nPos.yPos - nMinusOnePos.yPos;
-  FENEForces.FENE_y2 = (c.H * Q_y2) / (1 - (pow(Q_y2, 2) / pow (c.Q_0, 2)));
+  FENEForces.FENE_y2 = (c.H * Q_y2) / (1 - (pow(Q_y2, 2) / pow (c.MaxExtension, 2)));
 
   double Q_z1 = nPosPlusOne.zPos - nPos.zPos;
-  FENEForces.FENE_z1 = (c.H * Q_z1) / (1 - (pow(Q_z1, 2) / pow (c.Q_0, 2)));
+  FENEForces.FENE_z1 = (c.H * Q_z1) / (1 - (pow(Q_z1, 2) / pow (c.MaxExtension, 2)));
 
   double Q_z2 = nPos.zPos - nMinusOnePos.zPos;
-  FENEForces.FENE_z2 = (c.H * Q_z2) / (1 - (pow(Q_z2, 2) / pow (c.Q_0, 2)));
+  FENEForces.FENE_z2 = (c.H * Q_z2) / (1 - (pow(Q_z2, 2) / pow (c.MaxExtension, 2)));
 
   return FENEForces;
 }
