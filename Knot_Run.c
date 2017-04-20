a767cf96bc16e938fa375eb0324c9c6bf234421d
ollie318 <ow14948@my.bristol.ac.uk>
2017-04-20 15:41:32 +0100

    Change to run script

--- Knot_Run.c
+++ Knot_Run.c
@@ -6,15 +6,10 @@
 #include <sys/stat.h>
 
 int main(){
-    for(int v = 15; v <= 200; v += 5){
-        char buffer1[200];
-        sprintf(buffer1, "cd Output_Files2/Flow_Velocity/Vel%d\nqsub KnotVel%d\ncd ../../..", v, v);
-        system(buffer1);
-    }
 
     for(double eta = 1E-3; eta < 1E-1; eta+=1E-3){
         char buffer2[200];
-        sprintf(buffer2, "cd Output_Files2/Flow_Viscosity/Vel_100_Vis_%.3lf\nqsub KnotVis_%.3lf\ncd ../../..", eta, eta);
+        sprintf(buffer2, "cd Output_Files2/Flow_Viscosity/Vel_20_Vis_%.3lf\nqsub KnotVis_%.3lf\ncd ../../..", eta, eta);
         system(buffer2);
     }
 
